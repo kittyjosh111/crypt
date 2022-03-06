a1983df50d817280d87f45c6fb6416e1b3d0f4a0
joshuashi <joshua.shi@gmail.com>
2022-03-05 23:38:08 -0800

    remove shared key, reimplement some functions to match shortcut better

--- script.py
+++ script.py
@@ -5,15 +5,15 @@ from numpy import char
 
 #variables
 ascii_values = []
-d = ""
-e = ""
+encrypt = ""
 encode = "" 
+d = ""
 decoded = ""
 decrypted=""
 key = ""
-keyGen = ""
 finalKey = ""
 splitText = ""
+splitKey = ""
 
 #choose to encrypt or decrypt
 chooseFromMenu = int(input("Choose an option: [0]=Encrypt Message, [1]=Decrypt Message\n"))
@@ -22,33 +22,30 @@ chooseFromMenu = int(input("Choose an option: [0]=Encrypt Message, [1]=Decrypt M
 if chooseFromMenu == 0:
 
     providedInput = input("Input the message you want to encrypt:\n")
-    sharedKey = input("Input preset shared key between you and the recipient:\n")
     
     for character in providedInput:
-        #generates random number to multiply with the shared key and the string converted to ASCII
-        keyGen = random.randint(2,350)
-        finalKey = int(keyGen)*int(sharedKey)
-        e = int(finalKey)*int((ord(character))) #ord allows for string to ASCII
-        encode = str(encode) + str(e) + ":"
-        key = str(key) + str(finalKey) + ":"
-    key = key[:-1] #removes the final colon
+        #generates random number to addd to the string converted to ASCII
+        key = random.randint(2,750)
+        encrypt = int(key) + int((ord(character))) #ord allows for string to ASCII
+        encode = str(encode) + str(encrypt) + ":"
+        finalKey = str(finalKey) + str(key) + ":"
+
+    key = finalKey[:-1] #removes the final colon
     encode = encode[:-1] #removes the final colon
     print("\n- Your encrypted message is as follows -\n" + encode + "\n- Your key is as follows -\n" + key + "\n")
 
 else:
     try:
-        providedInput = input("Input the encrypted string you want to decrypt. It should only contain numbers and colons. (Ex = 111:2:333)\n")
+        providedInput = input("Input the encrypted message you want to decrypt. It should only contain numbers and colons. (Ex = 111:2:333)\n")
         key = input("Input your key. It should only contain numbers and colons. (Ex = 111:2:333)\n")
-        sharedKey = input("Input preset shared key between you and the recipient:\n")
-
+        
         splitText = providedInput.split(":")
         splitKey = key.split(":")
         i = 0
 
         #loops through for each item in split, analogous to 'repeat with each'
         while i < len(splitText):
-            key2 = int(splitKey[i])/int(sharedKey)
-            d = int(splitText[i])/int(key2)
+            d = int(splitText[i]) - int(splitKey[i])
             decoded = chr(int(d)) #chr allows for ASCII to string
             decrypted = str(decrypted) + str(decoded)
             i += 1
